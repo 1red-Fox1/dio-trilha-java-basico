6d6c946d810c3fd4f6e53450aae487b94e24bc30
RedFoX1029 <levinicolas2000@gmail.com>
2025-06-28 17:11:10 -0300

    [update] entrega da atividade conta bancaria

--- conta-banco/src/ContaTerminal.java
+++ conta-banco/src/ContaTerminal.java
@@ -1,5 +1,22 @@
+import java.util.Scanner;
+
 public class ContaTerminal {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+        Scanner input = new Scanner(System.in);
+
+        System.out.println("Digite o número da sua conta: ");
+        int numero = input.nextInt();
+        
+        System.out.println("Digite o número da sua agência: ");
+        String agencia = input.next();
+        
+        System.out.println("Digite o seu nome: ");
+        String nome = input.next();
+
+        System.out.println("Digite o valor do seu saldo: ");
+        double saldo = input.nextDouble();
+
+        System.out.printf("Olá %s, obrigado por criar uma conta em nosso banco, sua agência é %s, conta %d e seu saldo %.2f já está disponível para saque.", 
+                            nome, agencia, numero, saldo);
     }
 }
